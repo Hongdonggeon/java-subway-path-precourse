8c35ef1519059317fcf513d14ed25b368c1d702d
Hongdonggeon <ghd700@naver.com>
2021-12-17 14:18:29 +0900

    feat(line) : 이름이 같은 노선은 같은 노선으로 판정하기

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -2,6 +2,7 @@ package subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Line {
     private static final String STATION_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 최소 두글자 이상이여야 한다.";
@@ -24,5 +25,20 @@ public class Line {
         if (name.length() < 2) {
             throw new IllegalArgumentException(STATION_NAME_LENGTH_ERROR_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Line line = (Line)o;
+        return Objects.equals(name, line.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
