cd88cb24a5d75af1fe6d1cb7e3f45274e32a6c58
Hongdonggeon <ghd700@naver.com>
2021-12-17 13:44:32 +0900

    feat(station) : 역 이름 2글자 이상인지 검사하기

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,13 @@
 package subway.domain;
 
+import java.util.Objects;
+
 public class Station {
+    private static final String STATION_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 최소 두글자 이상이여야 한다.";
     private String name;
 
     public Station(String name) {
+        validateNameLength(name);
         this.name = name;
     }
 
@@ -11,5 +15,23 @@ public class Station {
         return name;
     }
 
-    // 추가 기능 구현
+    public static void validateNameLength(String name) {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException(STATION_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Station station = (Station)o;
+        return Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
